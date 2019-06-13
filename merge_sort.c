8ca171a8e5a1f445b205dfd14b000b7f1c8a0881
DanyEVO <daniel.valean2002@gmail.com>
2019-06-13 09:48:16 +0300

    Comments for main and devide functions.

--- merge_sort.c
+++ merge_sort.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-void devide(int min, int max, int arr[]);
-void merge(int min , int mid, int max, int arr[]);
+void devide(int min, int max, int arr[]);					
+// Recursive function that devides the array in smaller arrays until it has one element.
+void merge(int min , int mid, int max, int arr[]);			
+// The function which checks what element is greater and puts it in the rightful place.
 
 int main() 
 {
 	int n, i;
-	printf("How many elements does your array have? ");
+	printf("How many elements does your array have? ");		
+	// There is no tested limit for the array's size.
 	scanf(" %d", &n);
 
 	int arr[n];
@@ -23,7 +26,9 @@ int main()
 	}
 
 	printf("\n");
-	devide(0, n - 1, arr);
+	devide(0, n - 1, arr);									
+	// This is the call for the Recursive Function; 0 - allways the first index,
+	// n-1 - allways the last index & arr - the unsorted array.
 
 	printf("Sorted array: \n");
 	for(i=0; i<n; i++)
@@ -35,16 +40,23 @@ int main()
 void devide(int min, int max, int arr[])
 {
 	int mid;
-	if(min<max)
+	if(min<max)												
+	// When the values of min and max are equal the function will return to the previous one.
 	{	
-		mid = (min+max)/2;
+		mid = (min+max)/2;									
+		// mid - the closest centre of the array (it can have even number of elements,
+		// then it will choose the smallest from the two options).
 		devide(min, mid, arr);
 		devide(mid+1, max, arr);
 		merge(min, mid, max, arr);
+		// This is the call for the merging function, which will be called for every devided array;
+		// min - the first index, mid - the middle index, max - the last index & arr - the array with
+		// which the function will work.
 	}
 	else
 	{
-		return;
+		return; 
+		// Return to the previous function and continue.
 	}
 }
 
