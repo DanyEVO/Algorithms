748c3ddc8c936e80f8277e79b56b583cc23b4fc6
DanyEVO <daniel.valean2002@gmail.com>
2019-06-13 10:35:02 +0300

    Comments for merge function.

--- merge_sort.c
+++ merge_sort.c
@@ -63,27 +63,41 @@ void devide(int min, int max, int arr[])
 void merge(int min , int mid, int max, int arr[])
 {
 	int i = min;
+	// The first index of the first branch of the unsorted array.
 	int j = mid + 1;
+	// The first index of the second branch of the unsorted array.
 	int k = min;
+	// The first index of the temporal array.
 
 	int temp[max+1];
+	// The temporal array, which has max+1 elements, because max - the last index of the
+	// unsorted array, it starts from 0 => that the number of elements is with one unit bigger. 
 
-	while(i <= mid && j <= max)
+	while(i <= mid && j <= max) 
+	// It will execute until one array runs out of numbers.
 	{
 		if(arr[i]<=arr[j])
+		// Checks which element is smaller.
 		{
-			temp[k] = arr[i];
+			// If arr[i] is smaller, the temporal array will hold that value in the k slot,
+			// which increments itself in every loop.
+			temp[k] = arr[i]; 
 			k++;
 			i++;
+			// i is incremented to go to the next element of the first branch.
 		}
 		else
 		{
+			// The same goes here but when arr[j] is smaller.
 			temp[k] = arr[j];
 			k++;
 			j++;
+			// j is incremented to go to the next element of the second branch.
 		}
 	}
 
+	// Because the array is split in half, it cannot have more than one numbers in difference,
+	// meaning that just one of the following loops will be executed and just for one cycle.
 	while(i <= mid)
 	{
 		temp[k] = arr[i];
@@ -98,7 +112,11 @@ void merge(int min , int mid, int max, int arr[])
 		j++;
 	}
 
+	// Since the sorted values are saved in the temporal array they have to be transfered in the 
+	// unsorted array and avoid errors.
 	for(i=min; i<max+1; i++)
+	// i=min - not always does the temporal array start from the first element.
+	// i<max+1 - this is the max index value that the temporal array can have. 
 	{
 		arr[i] = temp[i];
 	}
